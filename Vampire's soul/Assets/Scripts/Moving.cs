b7da95936225a68b689bd5b80089ee52ef9ff527
MaciejG <12821744+Rodriqez@users.noreply.github.com>
2017-11-26 01:57:26 +0100

    Fixed moving
    added collision detection and rotating

--- Vampire's soul/Assets/Scripts/Moving.cs
+++ Vampire's soul/Assets/Scripts/Moving.cs
@@ -6,64 +6,80 @@ public class Moving : MonoBehaviour
 {
 
     public GameObject player;
+    public GameObject camerad;
+    private Rigidbody2D cr;
+    private Vector3 p;
+
+    private void Start()
+    {
+        cr = camerad.GetComponent<Rigidbody2D>();
+        p = player.transform.position;
+        cr.MovePosition(p);
+    }
 
     // Update is called once per frame
     void Update()
     {
-        Debug.DrawRay(player.transform.position, new Vector3(0, 0.17f, 0), Color.green);
-        Debug.DrawRay(player.transform.position, new Vector3(0, -0.17f, 0), Color.green);
-        Debug.DrawRay(player.transform.position, new Vector3(0.17f, 0, 0), Color.green);
-        Debug.DrawRay(player.transform.position, new Vector3(-0.17f, 0, 0), Color.green);
+        p = player.transform.position;
+
+        Vector3 up = new Vector3(p.x, p.y + 0.17f, p.z);
+        Vector3 down = new Vector3(p.x, p.y - 0.17f, p.z);
+        Vector3 left = new Vector3(p.x - 0.17f, p.y, p.z);
+        Vector3 right = new Vector3(p.x + 0.17f, p.y, p.z);
+
+        Debug.DrawRay(up, new Vector3(0, 0.17f, 0), Color.green);
+        Debug.DrawRay(down, new Vector3(0, -0.17f, 0), Color.green);
+        Debug.DrawRay(right, new Vector3(0.17f, 0, 0), Color.green);
+        Debug.DrawRay(left, new Vector3(-0.17f, 0, 0), Color.green);
 
         bool flagUp = true;
         bool flagDown = true;
         bool flagLeft = true;
         bool flagRight = true;
 
-        if (Physics2D.Raycast(player.transform.position, Vector2.up, 0.17f))
+        if (Physics2D.Raycast(up, Vector2.up, 0.17f))
         {
-            print("up");
-            //flagUp = false;
+            //print("up");
+            flagUp = false;
         }
-        if (Physics2D.Raycast(player.transform.position, Vector2.down, 0.17f))
+        if (Physics2D.Raycast(down, Vector2.down, 0.17f))
         {
-            print("down");
-            //flagDown = false;
+            //print("down");
+            flagDown = false;
         }
-        if (Physics2D.Raycast(player.transform.position, Vector2.left, 0.17f))
+        if (Physics2D.Raycast(left, Vector2.left, 0.17f))
         {
-            print("left");
-            //flagLeft = false;
+            //print("left");
+            flagLeft = false;
         }
-        if (Physics2D.Raycast(player.transform.position, Vector2.right, 0.17f))
+        if (Physics2D.Raycast(right, Vector2.right, 0.17f))
         {
-            print("right");
-            //flagRight = false;
+            //print("right");
+            flagRight = false;
         }
 
-
-        Vector3 p = player.transform.position;
         Rigidbody2D r = player.GetComponent<Rigidbody2D>();
 
-        if (Input.GetKeyDown("a") && flagLeft)
+        if (Input.GetKeyDown("a"))
         {
-            r.MovePosition(new Vector2(p.x - 0.32f, p.y));
-            print("a");
+            r.MoveRotation(90);
+            if(flagLeft) r.MovePosition(new Vector2(p.x - 0.32f, p.y));
         }
-        if (Input.GetKeyDown("d") && flagRight)
+       else if (Input.GetKeyDown("d"))
         {
-            r.MovePosition(new Vector2(p.x + 0.32f, p.y));
-            print("d");
+            r.MoveRotation(-90);
+            if (flagRight) r.MovePosition(new Vector2(p.x + 0.32f, p.y));
         }
-        if (Input.GetKeyDown("w") && flagUp)
+       else if (Input.GetKeyDown("w"))
         {
-            r.MovePosition(new Vector2(p.x, p.y + 0.32f));
-            print("w");
+            r.MoveRotation(0);
+            if (flagUp) r.MovePosition(new Vector2(p.x, p.y + 0.32f));
         }
-        if (Input.GetKeyDown("s") && flagDown)
+        else if (Input.GetKeyDown("s"))
         {
-            r.MovePosition(new Vector2(p.x, p.y - 0.32f));
-            print("s");
+            r.MoveRotation(180);
+            if (flagDown) r.MovePosition(new Vector2(p.x, p.y - 0.32f));
         }
+        cr.MovePosition(p);
     }
 }
