5a04a6bdcc8a6adf362955203aa611b57442361d
MisterGman <galem009@gmail.com>
2017-12-04 00:06:08 +0200

    Add files via upload
    That's all scripts that game that I bought has.
    It has some points to be checked.

---
+++ Vampire's soul/Assets/Scripts/Scripts/GameObject/ItemWeaponObj.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Weapons items, items that give you a weapon once the player get it on the floor.
+ */
+public class ItemWeaponObj : InteractiveObj{
+
+    //Data of the weapon
+    public WeaponBase weaponData;
+
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    //Once the player get it
+    public override void PlayerGet()
+    {
+        base.PlayerGet();
+        //get the weapon data that the player is using.
+        WeaponBase data = PlayerObj.playerInstance.GetWeapon();
+        //Give the weapon on the ground to the player
+        PlayerObj.playerInstance.SetWeapon(weaponData);
+
+        //Set this weapon to be the one the player was using
+        weaponData = data;
+        //Set the new texture for the weapon on the ground.
+        graphics.GetComponent<Renderer>().material.mainTexture = data.Image.texture;
+        
+    }
+}
---
+++ Vampire's soul/Assets/Scripts/Scripts/Util/SpawnChance.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * Set a chance that a object will be destroyed On Awake
+ */
+public class SpawnChance : MonoBehaviour {
+    //How much chance this object have to be spawned
+    [Range(0, 1f)]
+    public float chanceTospawn = 1f;
+
+    //The object will be spawned?
+    protected bool isSpawned;
+
+    protected virtual void Awake () {
+        //It will spawn?
+        isSpawned = Random.Range(0, 1f) < chanceTospawn;
+        //If not, destroy the object
+        if (!isSpawned)
+        {
+            Destroy(gameObject);
+        }
+    }
+	
+	
+}
