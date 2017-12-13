de92a1573fd690a76d6e603ae820b6e9602e3eb0
MisterGman <galem009@gmail.com>
2017-12-13 12:10:29 +0200

    Simple pathfinding and attack

--- Vampire's soul/Assets/Scripts/MG_Enemy.cs
+++ Vampire's soul/Assets/Scripts/MG_Enemy.cs
@@ -1,24 +1,47 @@
-﻿ using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MG_Enemy : MG_Person {
+public class MG_Enemy : MG_Person
+{
 
     //public Image health_bar;
-
+    public MG_Hero hero;
     private Moving m;
-    //private int maxHP = 100;
-    //public int hp = 100;
+
+
+    //private float maxHP = 100;
+    public float hp = 100;
+
+    //How far can find the player
+    public int distanceToSeePlayer = 5;
+    //How close it need to be to attack
+    public int distanceToAttack = 1;
+
+    //Position of enemy
+    public IntVector2 e_posit;
+
 
     private void Start()
     {
         m = scriptHolding.GetComponent<Moving>();
+        enemyInstance = this;
+        //Give initial position
+        e_posit = Position();
+        //  StartCoroutine(MovementTest());
     }
-
+    //Keep moving the NPC, for debugging
+    public IEnumerator MovementTest()
+    {
+        m.Move(this, Moving.Site.up);
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(MovementTest());
+    }
     //bool flagMove = false;
 
-    public override bool Move() {
+    public override bool Move()
+    {
         //Set();
         //m.SetPlayer(this);
         //m.SetUp();
@@ -27,5 +50,126 @@ public class MG_Enemy : MG_Person {
         else if (m.Move(this, Moving.Site.up)) ;
         else if (m.Move(this, Moving.Site.down)) ;
         return true;
+    }
+    public void AttackE(MG_Hero P, float dmg)
+    {
+        P.hp = P.hp - dmg;
+        if (P.hp <= 0)
+        {
+            Die(P);
+        }
+    }
+
+
+    //Enemy's turn to do action
+    public void OnnStep()
+    {
+        OnStep();
+        //Update position 
+        e_posit = Position();
+        //For each action it have, do a action
+        if (CanSeePlayer())
+        {
+            //If close to player
+            if (distanceToAttack == DistanceFromObject(hero))
+            {
+                AttackE(hero, 1); 
+                //Attack();
+            } else
+                //if you see but far away
+                m.Move(this, DirectionToObj(hero));
+        }
+
+        
+
+    }
+    //Can it see the player?
+    public bool CanSeePlayer()
+    {
+        Debug.Log(DistanceFromObject(hero));
+        return DistanceFromObject(hero) <= distanceToSeePlayer;
+    }
+
+
+    //Choose side where to go to get closer to enemy
+    public Moving.Site DirectionToObj(MG_Hero obj)
+    {
+
+        if (obj == null)
+            return Moving.Site.none;
+
+        IntVector2 position = Position();
+        IntVector2 dir = position - obj.posit;
+
+
+        //There will be two options to move, if can't go to opt1, will go to opt2
+        Moving.Site opt1;
+        Moving.Site opt2;
+
+        //If the direction X is closer than Z, choose left or right for the first option, and up or down for the second option
+        if (Mathf.Abs(dir.x) > Mathf.Abs(dir.y))
+        {
+            if (dir.x < 0)
+                opt1 = Moving.Site.right;
+            else
+                opt1 = Moving.Site.left;
+
+            if (dir.y < 0)
+                opt2 = Moving.Site.up;
+            else
+                opt2 = Moving.Site.down;
+        }
+        //Else, do the oposite
+        else
+        {
+            if (dir.y < 0)
+                opt1 = Moving.Site.up;
+            else
+                opt1 = Moving.Site.down;
+
+            if (dir.x < 0)
+                opt2 = Moving.Site.right;
+            else
+                opt2 = Moving.Site.left;
+        }
+        //If there is a obstacle for opt1, go to opt2.
+        //If there is a obstacle of opt2, the Move Func will not let him move, so he will stay in the same place;
+        //  if (!ObjectManager.CheckObstacle(position + Moving.SideToVector(opt1)))
+        return opt1;
+        //  else
+        //      return opt2;
+
+    }
+
+    //Calculate position on grid between 2 objectss
+    public int DistanceFromObject(MG_Person obj)
+    {
+
+        IntVector2 position = Position();
+        IntVector2 playerPos = obj.Position();
+      //  Debug.Log(position);
+       // Debug.Log(playerPos);
+        return Mathf.CeilToInt(Vector3.Distance(new Vector3(position.x, position.y), new Vector2(playerPos.x, playerPos.y)));
+    }
+
+    public void TakeDmg(float dmg)
+    {
+        hp -= dmg;
+        //    health_bar.fillAmount = (float)hp / maxHP;
+    }
+
+    protected override void Attack()
+    {
+        RaycastHit hit;
+        Vector3 site = Vector3.up;
+        Vector3 q = GetComponent<Transform>().eulerAngles;
+        if (q.z > -0.1 && q.z < 0.1) site = Vector3.up;
+        else if (q.z > 89 && q.z < 91) site = Vector3.left;
+        else if ((q.z > -91 && q.z < -89) || (q.z > 269 && q.z < 271)) site = Vector3.right;
+        else if (q.z > 179 && q.z < 181) site = Vector3.down;
+        if (Physics.Raycast(transform.position, site, out hit, 1f))
+        {
+            if (hit.collider.tag == "Player") hit.collider.gameObject.GetComponent<MG_Hero>().TakeDmg(10);
+        }
     }
 }
--- Vampire's soul/Assets/Scripts/MG_Hero.cs
+++ Vampire's soul/Assets/Scripts/MG_Hero.cs
@@ -1,38 +1,53 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class MG_Hero : MG_Person {
+public class MG_Hero : MG_Person
+{
 
     public Image health_bar;
 
     private Moving m;
-    private int maxHP = 100;
-    public int hp = 100;
+    //private float maxHP = 100;
+    public float hp = 100;
+
+    public MG_Enemy enemy;
+    public int atkRange = 1;
+
+    //Position of player on grid
+    public IntVector2 posit;
 
     private void Start()
     {
         m = scriptHolding.GetComponent<Moving>();
+        playerInstance = this;
+        //  pos = this.GetComponent<Transform>().position;
+
+        
     }
 
-    public void TakeDmg(int dmg)
+    public void TakeDmg(float dmg)
     {
-        hp -= dmg;
-        health_bar.fillAmount = (float)hp / maxHP;
+       hp -= dmg;
+    //    health_bar.fillAmount = (float)hp / maxHP;
     }
 
     bool flagMove = false;
 
-    public override bool Move() {
+    public override bool Move()
+    {
         //Set();
         //m.SetPlayer(this);
         //m.SetUp();
-        while (!flagMove) {
+        while (!flagMove)
+        {
 
             if (Input.GetKeyDown("a"))
             {
                 flagMove = m.Move(this, Moving.Site.left);
+                
             }
             else if (Input.GetKeyDown("d"))
             {
@@ -49,39 +64,145 @@ public class MG_Hero : MG_Person {
 
             if (flagMove)
             {
-                TakeDmg(1);  
+                TakeDmg(1);
             }
 
         }
+        InitStep();
         flagMove = !flagMove;
         return true;
     }
-    
+
+    //Attack enemy and check if hp==0 destroy
+    public void AttackP(MG_Enemy e, float dmg)
+    {
+        e.hp = e.hp - dmg;
+        if (e.hp <= 0)
+        {
+            Die(e);
+        }
+    }
+
+    //Let enemy make turn
+    public void InitStep()
+    {
+        //Send it to the MG_Person class to handle
+        UpdateStep();
+    }
+
     private void Update()
     {
-        if (Input.GetKeyDown("a"))
-        {
-            flagMove = m.Move(this, Moving.Site.left);
-        }
-        else if (Input.GetKeyDown("d"))
-        {
-            flagMove = m.Move(this, Moving.Site.right);
-        }
-        else if (Input.GetKeyDown("w"))
-        {
-            flagMove = m.Move(this, Moving.Site.up);
-        }
-        else if (Input.GetKeyDown("s"))
-        {
-            flagMove = m.Move(this, Moving.Site.down);
-        }
+        WallChecker();
 
-        if (flagMove)
+
+            if (atkRange != DistanceFromObject(enemy))
+            {
+                if (Input.GetKeyDown("a"))
+                {
+                    flagMove = m.Move(this, Moving.Site.left);
+                    InitStep();
+                }
+                else if (Input.GetKeyDown("d"))
+                {
+                    flagMove = m.Move(this, Moving.Site.right);
+                    InitStep();
+                }
+                else if (Input.GetKeyDown("w"))
+                {
+                    flagMove = m.Move(this, Moving.Site.up);
+                    InitStep();
+                }
+                else if (Input.GetKeyDown("s"))
+                {
+                    flagMove = m.Move(this, Moving.Site.down);
+                    InitStep();
+                } 
+            }
+            else
+            {
+                if (Input.GetKeyDown("f"))
+                {
+                    Attack();
+                    //AttackP(enemy, 50);
+                    //Debug.Log(atkRange + " asdadasd " + DistanceFromObject(enemy));
+                    InitStep();
+                }
+        }
+        
+             if (Input.GetKeyDown("k"))
+        {
+            flagMove = m.Move(this, Moving.Site.none);
+            InitStep();
+        }
+        //Give and update position of player
+        posit = Position();
+        if (flagMove) 
         {
             TakeDmg(1);
             flagMove = !flagMove;
         }
 
     }
-    
+
+    //Calculate position on grid between 2 objectss
+    public int DistanceFromObject(MG_Person obj)
+    {
+        if (obj == null)
+            return 999;
+
+        //Your position
+        IntVector2 position = Position();
+        //targer position
+        IntVector2 enemyPos = enemy.Position();
+      //  Debug.Log(position);
+       // Debug.Log(enemyPos);
+
+        //Calculating in int distance between two cells
+        return Mathf.CeilToInt(Vector3.Distance(new Vector3(position.x, position.y), new Vector2(enemyPos.x, enemyPos.y)));
+    }
+
+    public void WallChecker()
+    {
+        RaycastHit hit;
+        Vector3 site = Vector3.up;
+        Vector3 q = GetComponent<Transform>().eulerAngles;
+        if (q.z > -0.1 && q.z < 0.1) site = Vector3.up;
+        else if (q.z > 89 && q.z < 91) site = Vector3.left;
+        else if ((q.z > -91 && q.z < -89) || (q.z > 269 && q.z < 271)) site = Vector3.right;
+        else if (q.z > 179 && q.z < 181) site = Vector3.down;
+        if (Physics.Raycast(transform.position, site, out hit, 1f))
+        {
+            if (hit.collider.tag == "Wall")
+            {
+                flagMove = m.Move(this, Moving.Site.none);
+                InitStep();
+       
+            }
+        }
+    }
+
+    protected override void Attack()
+    {
+        RaycastHit hit;
+        Vector3 site = Vector3.up;
+        Vector3 q = GetComponent<Transform>().eulerAngles;
+        if (q.z > -0.1 && q.z < 0.1) site = Vector3.up;
+        else if (q.z > 89 && q.z < 91) site = Vector3.left;
+        else if ((q.z > -91 && q.z < -89) || (q.z > 269 && q.z < 271)) site = Vector3.right;
+        else if (q.z > 179 && q.z < 181) site = Vector3.down;
+        if (Physics.Raycast(transform.position, site, out hit, 1f))
+        {
+            if (hit.collider.tag == "Enemy")
+            {
+                hit.collider.GetComponent<MG_Enemy>().TakeDmg(30);
+                Debug.LogError("Atak");
+                if (enemy.hp <= 0)
+                {
+                    Die(enemy);
+                }
+            }
+        }
+    }
+
+
 }
