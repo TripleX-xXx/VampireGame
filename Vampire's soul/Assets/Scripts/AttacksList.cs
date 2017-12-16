b447f3db69234f809b50a42a8b1f984d2f532b62
MaciejG <12821744+Rodriqez@users.noreply.github.com>
2017-12-16 21:49:55 +0100

    Added attacks definition
    Added definition simple attak for enemy and player

--- Vampire's soul/Assets/Scripts/AttacksList.cs
+++ Vampire's soul/Assets/Scripts/AttacksList.cs
@@ -4,8 +4,39 @@ using UnityEngine;
 
 public static class AttacksList {
 
+    public static float EnemyAttack1(Person person)
+    {
+        RaycastHit hit;
+        Vector3 site = MG_Sides.SideToVector3(
+                        MG_Sides.EulerVectorToSide(person.transform.eulerAngles)
+                        );
+
+        if (Physics.Raycast(person.transform.position, site, out hit, 1f))
+        {
+            if (hit.collider.tag == "Player")
+            {
+                hit.collider.GetComponent<Person>().TakeDmg(10);
+            }
+        }
+        Debug.Log("EnemyAttack1");
+        return 0;
+    }
+
     public static float Attack1(Person person)
     {
+        RaycastHit hit;
+        Vector3 site = MG_Sides.SideToVector3(
+                        MG_Sides.EulerVectorToSide(person.transform.eulerAngles)
+                        );
+
+        if (Physics.Raycast(person.transform.position, site, out hit, 1f))
+        {
+            if (hit.collider.tag == "Enemy")
+            {
+                hit.collider.GetComponent<Person>().TakeDmg(30);
+                Debug.LogError("Atak");
+            }
+        }
         Debug.Log("Attack1");
         return 0;
     }
