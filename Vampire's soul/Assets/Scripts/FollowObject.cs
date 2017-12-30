98ecdd0e79c663fc71fbdc9ae2e84fc21e985c16
MaciejG <12821744+Rodriqez@users.noreply.github.com>
2017-12-30 01:09:40 +0100

    Follow system has been changed
    renamed FollowingCamera -> FollowObject
    now script need z value

--- Vampire's soul/Assets/Scripts/FollowObject.cs
+++ Vampire's soul/Assets/Scripts/FollowObject.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody))]
 
-public class FollowingCamera : MonoBehaviour {
+public class FollowObject : MonoBehaviour {
 
     public GameObject follow;
     public Vector3 camPosition;
@@ -20,6 +20,6 @@ public class FollowingCamera : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         p = follow.transform.position;
-        r.MovePosition(new Vector3(p.x + camPosition.x, p.y + camPosition.y, -10));
+        r.MovePosition(new Vector3(p.x + camPosition.x, p.y + camPosition.y, camPosition.z));
     }
 }
