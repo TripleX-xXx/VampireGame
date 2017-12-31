83c1140ca43086696d6c346972dc3b571e40b841
MaciejG <12821744+Rodriqez@users.noreply.github.com>
2017-12-31 21:09:38 +0100

    Small changes in Person and Hero

--- Vampire's soul/Assets/Scripts/Hero.cs
+++ Vampire's soul/Assets/Scripts/Hero.cs
@@ -5,10 +5,10 @@ public class Hero : Person {
 
     //Position of player on grid
     public IntVector2 posit;
-
+    public CanvasManager canvasMenager;
 
     private delegate float AttackType(Person p);
-    AttackType attack = null;
+    AttackType attack = AttacksList.Attack1;
 
     private void Update()
     {
@@ -17,77 +17,67 @@ public class Hero : Person {
         else if (Input.GetKeyDown("a")) Action(MG_Sides.Side.left);
         else if (Input.GetKeyDown("d")) Action(MG_Sides.Side.right);
         else if (Input.GetKeyDown("k")) Action(MG_Sides.Side.none); // skip round
-        else if (Input.GetKeyDown("1")) SetAbilitie(1); // choose skill 1
+        else if (Input.GetKeyDown("1")) SetAbilitie(3); // choose skill 1
         else if (Input.GetKeyDown("2")) SetAbilitie(2); // choose skill 2
-        else if (Input.GetKeyDown("3")) SetAbilitie(3); // choose skill 3
-        
+        else if (Input.GetKeyDown("3")) SetAbilitie(1); // choose skill 3
+        else if (Input.GetKeyDown("f")) Attack(); // Attack
+
     }
 
     private bool flagRoundEnd = false;
 
     private void Action(MG_Sides.Side side) // Key (wsadk) event control
     {
-        if (side == MG_Sides.Side.none) flagRoundEnd = true;
+        if (side == MG_Sides.Side.none) return;
         else if (side == MG_Sides.EulerVectorToSide(transform.eulerAngles))
         {
-            flagRoundEnd = true;
-            if (attack == null) flagRoundEnd = GetComponent<Moving>().Move(side);
-            else
-            {
-                TakeDmg(attack(this));
-                SetAbilitie(selectetAbilitie);
-            }
+            flagRoundEnd = GetComponent<Moving>().Move(side);
         }
-        else flagRoundEnd = GetComponent<Moving>().Move(side);
+        else
+        {
+            flagRoundEnd = false;
+            GetComponent<Moving>().Move(side);
+        }
 
         if(flagRoundEnd)
         {
             posit = Position();
             flagRoundEnd = false;
+            TakeDmg(1);
             InitStep();
+            canvasMenager.NextTour();
         }
 
     }
 
-    int selectetAbilitie = 0;
+    int selectetAbilitie = 1;
 
     private void SetAbilitie(int a) // set the selected skill. If it's the same then set movement mode
     {
-        if (selectetAbilitie == a) { attack = null; selectetAbilitie = 0; SetGraphicAbilitie(0); }
-        else if (a == 1) { attack = AttacksList.Attack1; selectetAbilitie = a; SetGraphicAbilitie(a); }
-        else if (a == 2) { attack = AttacksList.Attack2; selectetAbilitie = a; SetGraphicAbilitie(a); }
-        else if (a == 3) { attack = AttacksList.Attack3; selectetAbilitie = a; SetGraphicAbilitie(a); }
-    }
-
-    public Image Bite;
-    public Image Blink;
-    public Image Wave;
-
-    private void SetGraphicAbilitie(int a)
-    {
-        if (a == 0)
+        if (a == 1) // Defoult Attack must have coolDown == 0
         {
-            Bite.enabled = false;
-            Blink.enabled = false;
-            Wave.enabled = false;
-        }
-        else if (a == 1)
-        {
-            Bite.enabled = true;
-            Blink.enabled = false;
-            Wave.enabled = false;
+            selectetAbilitie = a;
+            attack = AttacksList.Attack1;
+            canvasMenager.SelectBite();
+            
         }
         else if (a == 2)
         {
-            Bite.enabled = false;
-            Blink.enabled = true;
-            Wave.enabled = false;
+            if (canvasMenager.SelectBlink())
+            {
+                selectetAbilitie = a;
+                attack = AttacksList.Attack3;
+            }
+            else SetAbilitie(1);
         }
         else if (a == 3)
         {
-            Bite.enabled = false;
-            Blink.enabled = false;
-            Wave.enabled = true;
+            if (canvasMenager.SelectWave())
+            {
+                selectetAbilitie = a;
+                attack = AttacksList.Attack2;
+            }
+            else SetAbilitie(1);
         }
     }
 
@@ -99,22 +89,28 @@ public class Hero : Person {
 
     protected override void Attack()
     {
-        RaycastHit hit;
-        Vector3 site = MG_Sides.SideToVector3(
-                        MG_Sides.EulerVectorToSide(transform.eulerAngles)
-                        );
+        TakeDmg(attack(this));
+        if (selectetAbilitie == 1) canvasMenager.UseBite();
+        if (selectetAbilitie == 2) canvasMenager.UseBlink();
+        if (selectetAbilitie == 3) canvasMenager.UseWave();
+        SetAbilitie(selectetAbilitie);
 
-        if (Physics.Raycast(transform.position, site, out hit, 1f))
-        {
-            if (hit.collider.tag == "Enemy")
-            {
-                hit.collider.GetComponent<Person>().TakeDmg(30);
-                Debug.LogError("Atak");
-            }
-        }
+        posit = Position();
+        InitStep();
+        canvasMenager.NextTour();
+
     }
 
-    protected override void Die() // things that happen while the object dies
+    public override void TakeDmg(float dmg)
+    {
+        currHP -= dmg;
+        if (currHP < 0) currHP = 0;
+        if (currHP > maxHP) currHP = maxHP;
+        canvasMenager.SetHealthBar(currHP / maxHP);
+        if (currHP == 0) Die();
+    }
+
+    protected override void Die()
     {
         Destroy(gameObject);
         //GameOver();
--- Vampire's soul/Assets/Scripts/Person.cs
+++ Vampire's soul/Assets/Scripts/Person.cs
@@ -5,21 +5,21 @@ using UnityEngine.UI;
 
 public abstract class Person : MonoBehaviour {
 
-    public Image health_bar; // graphic health indicator 
+    //public Image health_bar; // graphic health indicator 
     //(to implement in future)// protected Image lossOfHealth_bar; //graphic loss of health indicator
 
     protected float maxHP = 100; // the highest available level of health
     protected float currHP = 100; // current health
 
     //public abstract bool Action(); //formerly Move()
-    protected abstract void Attack(); // execution of the set attack  *deprecated*
+    protected abstract void Attack(); // execution of the set attack
 
     public virtual void TakeDmg(float dmg) //through this method object receives damage
     {
         currHP -= dmg;
         if (currHP < 0) currHP = 0;
         if (currHP > maxHP) currHP = maxHP;
-        health_bar.fillAmount = currHP / maxHP;
+        //health_bar.fillAmount = currHP / maxHP;
         if (currHP == 0) Die();
     }
 
