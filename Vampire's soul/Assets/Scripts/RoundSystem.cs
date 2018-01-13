4b86adcb6c5f9d9c6b7194103b16e56f062078b5
MaciejG <12821744+Rodriqez@users.noreply.github.com>
2018-01-13 02:27:16 +0100

    Added Spikes + prefab
    +GS Spikes implementation (fixed)
    +Spikes prefab

--- Vampire's soul/Assets/Scripts/RoundSystem.cs
+++ Vampire's soul/Assets/Scripts/RoundSystem.cs
@@ -7,10 +7,15 @@ public static class RoundSystem {
     public static void UpdateStep()
     {
         Enemy[] objs = Object.FindObjectsOfType<Enemy>();
+        Spikes[] trap = Object.FindObjectsOfType<Spikes>();
         //Fist update the enemies, they can move and attack.
         for (int i = 0; i < objs.Length; i++)
         {
             objs[i].OnnStep();
+        }
+        for (int i = 0; i < trap.Length; i++)
+        {
+            trap[i].OnnStep();
         }
     }
 
---
+++ Vampire's soul/Assets/Scripts/Spikes.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Spikes : MonoBehaviour {
+
+    public bool flagDebug = false;
+    public float cellSize = 1;
+
+    public void OnnStep()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position-Vector3.forward* (cellSize/2), transform.forward, out hit, cellSize))
+        {
+            if (hit.collider.gameObject.tag == "Player" || hit.collider.gameObject.tag == "Enemy")
+            {
+                hit.collider.GetComponent<Person>().TakeDmg(10);
+            }
+        }
+    }
+
+    void Update()
+    {
+        if(flagDebug) Debug.DrawRay(transform.position - Vector3.forward * (cellSize / 2), transform.forward * cellSize, Color.green);
+    }
+
+}
